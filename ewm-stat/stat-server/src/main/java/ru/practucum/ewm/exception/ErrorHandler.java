9446c53b425965279c705c02552aa65e777d92fa
Dozhin.V <dozhin.v@gmail.com>
2024-06-07 17:39:15 +0300

    refactor: move dependancies to child project's POM.xml

--- ewm-stat/stat-server/src/main/java/ru/practucum/ewm/exception/ErrorHandler.java
+++ ewm-stat/stat-server/src/main/java/ru/practucum/ewm/exception/ErrorHandler.java
@@ -2,8 +2,10 @@ package ru.practucum.ewm.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorHandler {
 
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handlerBadRequest(final BadRequestException e) {
@@ -20,12 +21,31 @@ public class ErrorHandler {
         return new ErrorResponse("Object not available 400 ", e.getMessage());
     }
 
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleIntegrityException(final DataAccessException e) {
         log.warn("409 {}", e.getMessage(), e);
         return new ErrorResponse("No valid data", e.getMessage());
     }
-}
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleThrowable(final MethodArgumentNotValidException e) {
+        log.info("400 {}", e.getMessage(), e);
+        return new ErrorResponse("MethodArgumentNotValid", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
+        log.error(e.getMessage());
+        return new ErrorResponse("ConstraintViolation", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.info("500 {}", e.getMessage(), e);
+        return new ErrorResponse("InternalServerError", e.getMessage());
+    }
+}
